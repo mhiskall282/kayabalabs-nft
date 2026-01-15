c48452170cf8f67fb28e012352a63658e30d0f41
John Okyere <112992966+mhiskall282@users.noreply.github.com>
2026-01-15 02:00:27 +0000

    Update KayabaAchievementNFT.sol

--- src/KayabaAchievementNFT.sol
+++ src/KayabaAchievementNFT.sol
@@ -2,12 +2,11 @@
 pragma solidity ^0.8.20;
 
 
-import "..lib/openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
-import "..lib/openzeppelin-contracts/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/utils/Strings.sol";
 
-import "..lib/openzeppelin-contracts/contracts/utils/Strings.sol";
-
-import "..lib/openzeppelin-contracts/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
 
 contract KayabaAchievementNFT is ERC721, ERC721URIStorage, Ownable {
     using Strings for uint256;
